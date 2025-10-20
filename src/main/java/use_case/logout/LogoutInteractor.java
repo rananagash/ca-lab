2f360874d24548d91b10b13ad88d3c34be9c4f85
Inba Thiyagarajan <inbathiyagarajan@Inbas-MacBook-Pro-5.local>
2025-10-20 16:06:02 -0400

    done

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -4,25 +4,20 @@ package use_case.logout;
  * The Logout Interactor.
  */
 public class LogoutInteractor implements LogoutInputBoundary {
-    private LogoutUserDataAccessInterface userDataAccessObject;
-    private LogoutOutputBoundary logoutPresenter;
+    private final LogoutUserDataAccessInterface userDataAccessObject;
+    private final LogoutOutputBoundary logoutPresenter;
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
         this.userDataAccessObject = userDataAccessInterface;
         this.logoutPresenter = logoutOutputBoundary;
     }
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
         String currentUsername = userDataAccessObject.getCurrentUsername();
         userDataAccessObject.setCurrentUsername(null);
         LogoutOutputData logoutOutputData = new LogoutOutputData(currentUsername);
         logoutPresenter.prepareSuccessView(logoutOutputData);
     }
 }
-    }
-}
-
