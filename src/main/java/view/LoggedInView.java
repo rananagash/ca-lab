d197506e446539b3a0949537398c466758f567f0
rananagash <rana.nagash@mail.utoronto.ca>
2025-10-20 15:50:16 -0400

    LoggedInView TODO

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -109,6 +109,9 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      */
     public void actionPerformed(ActionEvent evt) {
         // TODO: execute the logout use case through the Controller
+        if (evt.getSource().equals(logOut)) {
+            logoutController.execute();
+        }
         System.out.println("Click " + evt.getActionCommand());
     }
 
@@ -141,5 +144,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
 
     public void setLogoutController(LogoutController logoutController) {
         // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
